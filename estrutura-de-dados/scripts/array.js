0505044f60b61de29e59203a912b167e1e9a2013
Guilherme S <guinho.shuto@gmail.com>
2022-12-11 22:16:02 -0400

    css filters

---
+++ estrutura-de-dados/scripts/array.js
@@ -0,0 +1,19 @@
+const avgTemp = []
+
+avgTemp[0] = 31.9
+avgTemp[1] = 35.3
+avgTemp[2] = 42
+avgTemp[3] = 38
+avgTemp[4] = 25.5
+
+const daysOfWeek = new Array(
+    "Sunday", 
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+)
+
+console.log(daysOfWeek[2])
