cdc7716bd502b1ca7a540f40b5a2cf6672cb097d
Guilherme Shuto <guinho.shuto@gmail.com>
2023-07-06 11:43:34 -0400

    bora codar

--- boraCodar/20-29/26/main.js
+++ boraCodar/20-29/26/main.js
@@ -1,5 +1,6 @@
-console.log('oi')
 let counter = 1;
+const ingredientsQtd = document.getElementsByClassName('qtd')
+console.log(typeof ingredientsQtd)
 const portions = document.querySelector('#counter')
 
 function formatNumber(n){
@@ -7,5 +8,18 @@ function formatNumber(n){
 }
 
 function changePortion(n){
-    console.log(n)
+    counter += n
+    if(counter < 0) counter = 0
+    setPortion()
 }
+
+function setPortion(){
+    Array.prototype.forEach.call(ingredientsQtd , e => {
+        e.innerHTML = parseInt(e.dataset.qtd)*counter; 
+    })
+    portions.innerHTML = formatNumber(counter)
+} 
+
+
+setPortion();
+
