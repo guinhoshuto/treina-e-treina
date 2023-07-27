930078c12fe01567ac8178b06df5004f89f0a2b8
Guilherme Shuto <guinho.shuto@gmail.com>
2023-07-27 11:27:51 -0400

    bora codar 29

--- boraCodar/20-29/29/script.js
+++ boraCodar/20-29/29/script.js
@@ -1,7 +1,9 @@
 const slider = document.getElementById("slider")
 const before = document.querySelector(".before-image")
+const dragger = document.getElementById("dragger")
 
 slider.addEventListener("input", (e) => {
     console.log(e.target.value)
     before.style.width = e.target.value + '%'  
+    dragger.style.left = e.target.value + '%'  
 })
