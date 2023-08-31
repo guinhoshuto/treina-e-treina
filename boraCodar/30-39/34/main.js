a0d82e4e07306a9cc5d6d670ecdb9199f293d551
Guilherme Shuto <guinho.shuto@gmail.com>
2023-08-31 14:23:19 -0400

    bora codar 34 - select menu

--- boraCodar/30-39/34/main.js
+++ boraCodar/30-39/34/main.js
@@ -1,1 +1,33 @@
 lucide.createIcons()
+
+let select = document.querySelector('.select')
+let selectedValue = document.querySelector('#selected-value')
+let optionsViewButton = document.querySelector('#options-view-button')
+let inputsOptions = document.querySelectorAll('.option input')
+
+inputsOptions.forEach(input => {
+    input.addEventListener('click', event => {
+        selectedValue.textContent = input.dataset.label
+
+        const isMouseOrTouch = event.pointerType === "mouse" || event.pointerType === "touch"
+        isMouseOrTouch && optionsViewButton.click()
+    })
+})
+
+window.addEventListener('keydown', event => {
+    if(!select.classList.contains('open')) return
+
+    if(event.key === "Escape" || event.key === " "){
+        console.log('oi')
+        optionsViewButton.click()
+    }
+})
+
+optionsViewButton.addEventListener('input', () => {
+    select.classList.toggle('open')
+    if(!select.classList.contains('open')) return
+
+    const input = document.querySelector('.option input:checked') || document.querySelector('.option input')
+    console.log('ois')
+    input.focus()
+})
